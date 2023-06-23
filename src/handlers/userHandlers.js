d0e3ec9cefb6ceddb16806ffc1962f2471e304ae
Cindy Posada <cindy@vauxoo.com>
2023-06-23 18:31:03 -0500

    [refactor] booksApi: improved error handling in login route

--- src/handlers/userHandlers.js
+++ src/handlers/userHandlers.js
@@ -16,11 +16,11 @@ const signupHandler = async (req, res) => {
 const loginHandler = async (req, res) => {
   const { email, password } = req.body;
 
-  const user = await User.findOne({ where: { email } });
-  if (!user) throw new Error("Email is not registered");
-  if (user.password !== password) throw new Error("Wrong password");
-
   try {
+    const user = await User.findOne({ where: { email } });
+    if (!user) throw new Error("Email is not registered");
+    if (user.password !== password) throw new Error("Wrong password");
+
     jwt.sign({ user }, "secretkey", (err, token) => {
       res.json({
         token,
@@ -45,6 +45,7 @@ const changePasswordHandler = async (req,res) => {
     res.status(400).json({ error: error.message });
   }
 }
+
 module.exports = {
   signupHandler,
   loginHandler,
